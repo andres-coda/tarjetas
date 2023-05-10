41feb0cdd9afd727d82f8d48c77051b49469fd07
andres-coda <codaandres@gmail.com>
2023-05-09 22:57:01 -0300

    Ejercicio casi terminado- preentrega

---
+++ index.js
@@ -0,0 +1,18 @@
+import data from "./data.js";
+const objeto = JSON.parse(data);
+
+const container = document.getElementById("conteiner");
+    for (let e of objeto) {
+        const card= document.createElement("div");
+        card.classList.add("tarjeta");
+        card.innerHTML =`
+        <img src=${e.picture.large} alt=${e.name.first} ${e.name.last}/>
+        <div class="texto">
+        <h2> ${e.name.first} ${e.name.last}<h2>
+        <p>${e.location.street.name} ${e.location.street.number}</p> 
+        </div>
+        `;
+        conteiner.appendChild(card);
+    }
+
+
